26bdfa82157538925355e2312bf64d6b06cffc6a
Marcin Sowa <marcin.sowa121@gmail.com>
2020-02-29 16:30:51 +0100

    correct incrementation

--- src/pierwsze_programy/Petle.java
+++ src/pierwsze_programy/Petle.java
@@ -8,7 +8,7 @@ public class Petle {
 
         while (licznikWyswietlen < iloscWyswietlen){
             System.out.println("Hello World");
-            licznikWyswietlen--;
+            licznikWyswietlen++;
         }
 
     }
